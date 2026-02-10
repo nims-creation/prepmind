5823f823932e880b67ff122dcafd24288ba7f5ec
Nims-Creation <nimscreation06@gmail.com>
2026-02-10 18:06:13 +0530

    Adding the Ratelimiter in the project mean how many time can user and Admin request from the openAi

--- src/main/java/com/nimscreation/prepmind/ai/service/impl/AiServiceImpl.java
+++ src/main/java/com/nimscreation/prepmind/ai/service/impl/AiServiceImpl.java
@@ -6,14 +6,27 @@ import com.nimscreation.prepmind.ai.service.AiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class AiServiceImpl implements AiService {
 
     private final AiProvider aiProvider;
+    private final AiRateLimiter rateLimiter;
 
     @Override
     public String generate(AiUseCase useCase, String prompt) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        String userId = auth.getName();
+        boolean isAdmin = auth.getAuthorities()
+                .stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+
+        if (!rateLimiter.allowRequest(userId, isAdmin)) {
+            throw new TooManyRequestsException("AI rate limit exceeded");
+        }
+
         return aiProvider.generate(useCase, prompt);
     }
 }
